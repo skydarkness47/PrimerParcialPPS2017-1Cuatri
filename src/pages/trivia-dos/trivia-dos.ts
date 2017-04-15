1971125836469311d3cd15755fffa31230e41df9
skydarkness47 <yonathanaguirre@live.com>
2017-04-15 10:40:33 -0300

    odio eterno al onload
    no funciona propiedad onload para modificar parametros :-1:

--- src/pages/trivia-dos/trivia-dos.ts
+++ src/pages/trivia-dos/trivia-dos.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import {TriviaUno} from '../trivia-uno/trivia-uno';
 
 @Component({
   selector: 'page-trivia-dos',
   templateUrl: 'trivia-dos.html',
 })
 export class TriviaDos {
+
+
   constructor(public navCtrl: NavController) {
   }
-
 
 }
--- src/pages/trivia-uno/trivia-uno.ts
+++ src/pages/trivia-uno/trivia-uno.ts
@@ -2,21 +2,37 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { TriviaDos } from '../trivia-dos/trivia-dos';
 
+interface Alber { something: string; }
 
 @Component({
   selector: 'page-trivia-uno',
   templateUrl: 'trivia-uno.html',
 })
 export class TriviaUno {
+
+ preguntas : number;
+ trivia : string;
+  opcion1 = document.getElementById('opcion1');
+  opcion2 = document.getElementById("opcion2");
+  opcion3 = document.getElementById("opcion3");
+ 
+
+ 
   constructor(public navCtrl: NavController) {
   }
-Opciones(dato){
+
+Opciones(){
+
+  console.log("hols");
+  if(this.preguntas == null)
+  {
+    this.trivia = "¿En que año se celebro el pacto de olivos";
+    this.opcion1.setAttribute("value", "en 1995");
+    this.opcion2.setAttribute("value","en 1994");
+    this.opcion3.setAttribute("value", "en 1996");
     
-
-
-      if(dato == "2"){
-          this.navCtrl.setRoot(TriviaDos);
-      }
+  }
+     
   
   }
 
