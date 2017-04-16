aeac6ced0dc8acf2a4d8f8a58a08d1959d6d8370
skydarkness47 <yonathanaguirre@live.com>
2017-04-16 16:51:48 -0300

    solucion del onload y logica terminada
    logica de la trivia terminada, y solucion del onload con el lyfeevent
    ionViewDidLoad

--- src/pages/trivia-uno/trivia-uno.ts
+++ src/pages/trivia-uno/trivia-uno.ts
@@ -12,28 +12,73 @@ export class TriviaUno {
 
  preguntas : number;
  trivia : string;
-  opcion1 = document.getElementById('opcion1');
-  opcion2 = document.getElementById("opcion2");
-  opcion3 = document.getElementById("opcion3");
- 
+puntuacion: number;
+ reCorrecta: number;
 
  
   constructor(public navCtrl: NavController) {
   }
 
-Opciones(){
-
-  console.log("hols");
-  if(this.preguntas == null)
-  {
-    this.trivia = "¿En que año se celebro el pacto de olivos";
-    this.opcion1.setAttribute("value", "en 1995");
-    this.opcion2.setAttribute("value","en 1994");
-    this.opcion3.setAttribute("value", "en 1996");
-    
+  ionViewDidLoad(){
+    this.trivia = "¿En que año se celebro el pacto de olivos?";
+    document.getElementById("opcion1").setAttribute("value","en 1990");
+    document.getElementById("opcion2").setAttribute("value","en 1995");
+    document.getElementById("opcion3").setAttribute("value","en 1994");   
+    this.puntuacion = 0; 
+    this.reCorrecta = 3;
+    this.preguntas = 1;
   }
+  preguntaDos(){
+     this.trivia = "¿En que año argentina salio campeon del mundo?";
+    document.getElementById("opcion1").setAttribute("value","en 1810");
+    document.getElementById("opcion2").setAttribute("value","en 1994");
+    document.getElementById("opcion3").setAttribute("value","en 1984");  
      
+  }
+  preguntaTres(){
+      this.trivia = "¿cuando cumple ioni?";
+    document.getElementById("opcion1").setAttribute("value","en 1995");
+    document.getElementById("opcion2").setAttribute("value","en 1996");
+    document.getElementById("opcion3").setAttribute("value","en 1997");  
+  }
+
+VerificarPregunta(respuesta){
+
+    if(this.preguntas == 1){
+       if(respuesta == 3)
+          {
+            this.puntuacion = this.puntuacion+1;
+            this.preguntas = this.preguntas+1;
+            this.preguntaDos();
+          }else{
+            this.preguntas = this.preguntas+1;
+            this.preguntaDos();
+          }
+     }else if (this.preguntas == 2){
+       console.log("estoy en respuesta",respuesta);
+             if(respuesta == 3)
+                {
+                  this.puntuacion = this.puntuacion+1;
+                  this.preguntas = this.preguntas+1;
+                  this.preguntaTres();
+                }else{
+                  this.preguntas = this.preguntas+1;
+                  this.preguntaTres();
+                }
+     }else if (this.preguntas ==3){
+             if(respuesta == 1)
+                {
+                  this.puntuacion = this.puntuacion+1;
+                  this.preguntas = this.preguntas+1;
+                  console.info(this.puntuacion);
+                }else{
+                  this.preguntas = this.preguntas+1;
+                 console.info(this.puntuacion);
+                }
+     }
   
   }
+
+
 
 }
